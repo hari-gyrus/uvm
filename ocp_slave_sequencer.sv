48fbc1105677c41d84c883bed97d86c79c398ca5
Hari.K <24235169+hari-gyrus@users.noreply.github.com>
2017-12-18 14:25:46 -0800

    save work

--- ocp_slave_sequencer.sv
+++
@@ -1,9 +0,0 @@
-// ********************************************************************************
-// Copyright (c) 2017-2018: Gyrus, Inc
-// 
-// This file can not be copied and/or distributed without the express permission 
-// of Gyrus, Inc. Subject to terms of license agreement - Check "LICENSE" which 
-// comes with this distribution for more information.
-// ********************************************************************************
-
-  
